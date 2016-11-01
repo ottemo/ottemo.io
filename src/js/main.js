af7e6ea676968b3c771a2e3de4cfdefceaa7cbc6
Evgeniy Kosten <ekosten@speroteck.com>
2016-11-01 12:17:00 +0200

    Home page content added

---
+++ src/js/main.js
@@ -0,0 +1,25 @@
+/**
+ * Created by Evgeniy on 28.10.2016.
+ */
+$(document).ready(function() {
+
+    $("#owl-demo").owlCarousel({
+
+        navigation : true, // Show next and prev buttons
+        slideSpeed : 300,
+        paginationSpeed : 400,
+        singleItem:true
+    });
+
+    $.scrollify({
+        section : ".scroll-to-section",
+        interstitialSection : ".footer",
+        easing: "easeOutExpo",
+        scrollSpeed: 1100,
+        offset : 0,
+        scrollbars: true,
+        setHeights: true,
+        overflowScroll: true
+    });
+
+});
